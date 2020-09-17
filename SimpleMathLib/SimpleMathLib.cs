a20e24be837c2d6ad1f16a9413e4aeb02bc728eb
Brian Blackman <brian_blackman@hotmail.com>
2020-09-17 15:53:14 -0400

    Initial Project creation

---
+++ SimpleMathLib/SimpleMathLib.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+
+namespace SimpleMathLib
+{
+    public class MathClass
+    {
+
+        // Add two numbers together
+        public int Add(int a, int b)
+        {
+            if (a < 1)
+                return 0;
+            return a + b;
+        }
+
+        // Add two doubles together
+        public double Add(double a, double b)
+        {
+            if (a < 1)
+                return 0;
+            return a + b;
+        }
+        // Subtract two numbers
+        public int Subtract(int a, int b)
+        {
+            if (a < 1)
+                return 0;
+            return (a - b);
+        }
+
+        // Multiply two numbers
+        public int Multiply(int a, int b)
+        {
+            return (a * b);
+        }
+
+        public int Divide(int a, int b)
+        {
+            return (a / b);
+        }
+
+    }
+}
